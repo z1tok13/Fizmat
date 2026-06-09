2fcc2ccfb2808029beb528a9d7e4ce5286190d00
z1tok13 <timosha300111@gmail.com>
2026-06-09 12:00:21 +0400

    закончили

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Fractions import Fractions
 from Temerature import Temperature
+from Converter import Converter
 # numerator = int(input("Введите значение числителя: "))
 # denominator = int(input("Введите значение знаменателя: "))
 # fraction = Fractions(numerator, denominator)
@@ -13,5 +14,10 @@ from Temerature import Temperature
 # print((fraction.div(fraction2)).display())
 
 
-print(Temperature.cel_fa(32))
-print(Temperature.fa_cel(78))
+# print(Temperature.cel_fa(32))
+# print(Temperature.fa_cel(78))
+
+print(Converter.km_mil(20))
+print(Converter.mil_km(50))
+print(Converter.gal_lit(30))
+print(Converter.lit_gal(60))
