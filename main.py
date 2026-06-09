964087b6cd57e4a43bb603444ebef154d95b27a6
z1tok13 <timosha300111@gmail.com>
2026-06-09 11:53:13 +0400

    проверили работоспособность конвертера температур

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from Fractions import Fractions
+from Temerature import Temperature
+# numerator = int(input("Введите значение числителя: "))
+# denominator = int(input("Введите значение знаменателя: "))
+# fraction = Fractions(numerator, denominator)
 
-numerator = int(input("Введите значение числителя: "))
-denominator = int(input("Введите значение знаменателя: "))
-fraction = Fractions(numerator, denominator)
+# numerator = int(input("Введите значение числителя: "))
+# denominator = int(input("Введите значение знаменателя: "))
+# fraction2 = Fractions(numerator, denominator)
+# print((fraction.add(fraction2)).display())
+# print((fraction.sub(fraction2)).display())
+# print((fraction.mult(fraction2)).display())
+# print((fraction.div(fraction2)).display())
 
-numerator = int(input("Введите значение числителя: "))
-denominator = int(input("Введите значение знаменателя: "))
-fraction2 = Fractions(numerator, denominator)
-print((fraction.add(fraction2)).display())
-print((fraction.sub(fraction2)).display())
-print((fraction.mult(fraction2)).display())
-print((fraction.div(fraction2)).display())
+
+print(Temperature.cel_fa(32))
+print(Temperature.fa_cel(78))
