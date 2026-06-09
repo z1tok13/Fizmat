14c7b731444a7ea7e6a0956319e532e3b3d0a111
z1tok13 <timosha300111@gmail.com>
2026-06-09 11:57:46 +0400

    создали класс конвертер

---
+++ Converter.py
@@ -0,0 +1,14 @@
+class Converter:
+    @staticmethod
+    def km_mil(km):
+        return km * 0.621371
+    @staticmethod
+    def mil_km(mil):
+        return mil * 1.60934
+
+    @staticmethod
+    def gal_lit(gal):
+        return gal * 3.78541
+    @staticmethod
+    def lit_gal(lit):
+        return lit * 0.264172
--- Temerature.py
+++ Temerature.py
@@ -2,5 +2,6 @@ class Temperature:
     @staticmethod
     def cel_fa(cel):
         return (cel * 9 / 5) + 32
+    @staticmethod
     def fa_cel(fa):
         return (fa - 32) * 5 / 9
